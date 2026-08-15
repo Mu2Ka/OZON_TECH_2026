3e4531e7c73d026870667e9a5627591ff58a37b9
Vladislav Fedoseev <myskagg1@gmail.com>
2026-08-16 01:48:03 +0700

    написал код для обучения на полной хрени и  для предсказания итогового

--- neural-network-rnn-lstm-gru/training_model.py
+++ neural-network-rnn-lstm-gru/training_model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import torch
+from torch import nn
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 import numpy as np
@@ -100,3 +101,60 @@ def pytorch_model_validation(
     model.load_state_dict(best_model)
     model.eval()
     return model, train_losses, valid_losses
+
+
+def pytorch_model_fit(model, train_files, epochs):
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    train_loader =  create_dataloader_dataset(train_files)
+    train_losses = []
+
+    for epoch in range(epochs):
+        model.train()
+        train_loss_sum = 0
+
+        for X_batch, y_batch in train_loader:
+            optimizer.zero_grad()
+            predictions = model(X_batch).reshape(-1)
+            loss = criterion(predictions, y_batch)
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+            optimizer.step()
+            train_loss_sum += loss.item() * len(X_batch)
+
+        train_loss = train_loss_sum / len(train_loader.dataset)
+        train_losses.append(train_loss)
+
+        if epoch == 0 or (epoch + 1) % 10 == 0 or epoch + 1 == epochs:
+            print(
+                f"Final epoch {epoch + 1:03d} | "
+                f"train={train_loss:.6f}"
+            )
+
+    model.eval()
+    return model, train_losses
+
+
+def predict_model(model, test_files):
+    test_loader = create_dataloader_dataset(test_files)
+    criterion = nn.MSELoss()
+
+    model.eval()
+    test_loss_sum = 0
+    predictions_list = []
+    targets_list = []
+
+    with torch.inference_mode():
+        for X_batch, y_batch in test_loader:
+            predictions = model(X_batch).reshape(-1)
+            loss = criterion(predictions, y_batch)
+            test_loss_sum += loss.item() * len(X_batch)
+
+            predictions_list.append(predictions.cpu().numpy())
+            targets_list.append(y_batch.cpu().numpy())
+
+    test_loss = test_loss_sum / len(test_loader.dataset)
+    test_predictions = np.concatenate(predictions_list)
+    test_targets = np.concatenate(targets_list)
+
+    return test_loss, test_predictions, test_targets
